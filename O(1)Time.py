d29cbe14ded0d347e74119c8a0873e790b13d017
suhana0709 <farjanajaman1975@gmail.com>
2025-10-17 07:21:25 -0500

    Add files via upload

---
+++ O(1)Time.py
@@ -0,0 +1,9 @@
+def printnumber(n):
+    iteration = 0
+    print("The number entered by the user is :- ",n)
+    iteration += 1
+    print("Total iteration done by the computer is:- ",iteration)
+    print("\n")
+
+printnumber(10)
+printnumber(200)
