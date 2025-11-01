ec7f321f41a10c267cc3b9584c78f46dfaf1b52e
suhana0709 <farjanajaman1975@gmail.com>
2025-11-01 07:32:07 -0500

    Add files via upload

---
+++ CheckIfPrime.py
@@ -0,0 +1,15 @@
+#prime number checker
+print("\n#*******   Prime Number Checker   *********#\n")
+
+from math import sqrt
+num = int(input("Enter the number: "))
+
+if num>1:
+    for i in range (2, int(sqrt(num)) + 1):
+        if (num%i == 0):
+            print(num,("is not a prime number."))
+            break
+        else:
+            print(num,"is a prime number.")
+else:
+    print(num," is not a prime number.")
